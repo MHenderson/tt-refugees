e0b81a3495ff428a2e2b3ff633a7c271f0ebe87c
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-09-06 08:41:58 +0000

    add demo plotting script

---
+++ example.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(ggplot2)
+library(targets)
+library(tidyr)
+
+# tar_make()
+
+X <- tar_read(population)
+
+X <- X |>
+  mutate(
+    year = as.Date(paste(year, 1, 1, sep = "-"))
+  )
+
+X_tidy <- X |>
+  pivot_longer(refugees:hst)
+
+XX <- X_tidy |>
+  filter(coo_name == "Afghanistan") |>
+  filter(coa_name == "Australia")
+
+ggplot(XX) +
+  geom_line(aes(year, value, colour = name))
