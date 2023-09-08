c43dce90c1d367ef8a783de1d58a8a391f4e1028
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-09-08 13:19:18 +0000

    create targets pipeline for existing plots

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("ggplot2", "readr", "tibble"), # packages that your targets need to run
+  packages = c("dplyr", "ggplot2", "ggrepel", "readr", "tibble", "tidyr"), # packages that your targets need to run
   format = "rds" # default storage format
   # Set other options as needed.
 )
@@ -21,7 +21,7 @@ options(clustermq.scheduler = "multicore")
 # Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
 
 # Run the R scripts in the R/ folder with your custom functions:
-tar_source()
+# tar_source()
 # source("other_functions.R") # Source other scripts as needed. # nolint
 
 list(
@@ -30,12 +30,55 @@ list(
     command = read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-08-22/population.csv')
   ),
   tar_target(
-    name = plot,
+    name = population_pp,
+    command = population |> mutate(year = as.Date(paste(year, 1, 1, sep = "-")))
+  ),
+  tar_target(
+    name = tidy_population,
+    command = population_pp |> pivot_longer(refugees:hst)
+  ),
+  tar_target(
+    name = test_plot,
     command = ggplot(data = population) + geom_point(aes(x = year, y = asylum_seekers))
   ),
   tar_target(
-    name = save_plot,
-    command = ggsave(plot = plot, filename = "plot/plot.png", bg = "white", width = 10, height = 8),
+    name = save_test_plot,
+    command = ggsave(plot = test_plot, filename = "plot/test-plot.png", bg = "white", width = 10, height = 8),
+    format = "file"
+  ),
+  tar_target(
+    name = afg_aus_plot,
+    command = {
+      tidy_population |>
+        filter(coo_name == "Afghanistan") |>
+        filter(coa_name == "Australia") |>
+        ggplot() +
+          geom_line(aes(year, value, colour = name))
+    }
+  ),
+  tar_target(
+    name = save_afg_aus_plot,
+    command = ggsave(plot = afg_aus_plot, filename = "plot/afg-aus-plot.png", bg = "white", width = 10, height = 8),
+    format = "file"
+  ),
+  tar_target(
+    name = afg_asylum_plot,
+    command = {
+      tidy_population |>
+        filter(coo_name == "Afghanistan") |>
+        filter(name == "asylum_seekers") |>
+        ggplot(aes(year, value)) +
+          geom_line(aes(colour = coa_name)) +
+          theme(legend.position = "none") +
+          geom_text_repel(
+            aes(label = coa_name), data = function(x) x |> filter(year == "2021-01-01"),
+            fontface ="plain", color = "black", size = 3
+          )
+    }
+  ),
+  tar_target(
+    name = save_afg_asylum_plot,
+    command = ggsave(plot = afg_asylum_plot, filename = "plot/afg-asylum-plot.png", bg = "white", width = 10, height = 8),
     format = "file"
   )
 )
