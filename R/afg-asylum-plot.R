8676afcc2fc935fbe7dcabeeeb68efccbed150cf
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-09-14 14:46:22 +0100

    add new data targets for existing plots (#17)

--- R/afg-asylum-plot.R
+++ R/afg-asylum-plot.R
@@ -1,12 +1,18 @@
+afg_asylum_data <- function(X) {
+  X |>
+    filter(coo_name == "Afghanistan") |>
+    filter(name == "asylum_seekers")
+}
+
 afg_asylum_plot <- function(X) {
-   X |>
-        dplyr::filter(coo_name == "Afghanistan") |>
-        dplyr::filter(name == "asylum_seekers") |>
-        ggplot2::ggplot(aes(year, value)) +
-          ggplot2::geom_line(ggplot2::aes(colour = coa_name)) +
-          ggplot2::theme(legend.position = "none") +
-          ggrepel::geom_text_repel(
-            ggplot2::aes(label = coa_name), data = function(x) x |> filter(year == "2021-01-01"),
-            fontface ="plain", color = "black", size = 3
-          )
+  ggplot(X, aes(year, value)) +
+    geom_line(aes(colour = coa_name)) +
+    theme(legend.position = "none") +
+    geom_text_repel(
+      data = function(x) x |> filter(year == "2021-01-01"),
+      aes(label = coa_name),
+      fontface ="plain",
+      color = "black",
+      size = 3
+    )
 }
