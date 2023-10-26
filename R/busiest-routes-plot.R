e9ead365958ea00a361555e84472b3d2b55256c7
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-10-26 15:44:12 +0100

    plot other routes as grey lines (#28)

--- R/busiest-routes-plot.R
+++ R/busiest-routes-plot.R
@@ -2,35 +2,39 @@ busiest_routes_plot <- function(X) {
 
   X_asylum <- X |>
     filter(name == "asylum_seekers") |>
+    filter(coo_name != "Unknown") |>
     mutate(route = paste0(coo_name, "-", coa_name))
   
-  busiest_routes <- X_asylum |>
+  routes <- X_asylum |>
     group_by(route) |>
     summarise(value = sum(value)) |>
-    arrange(desc(value)) |>
-    head(6)
+    arrange(desc(value))
+  
+  busiest_routes <- routes |>
+    head(10)
+  
+  other_routes <- routes |>
+    slice(11:50)
   
   X_busiest_routes <- X_asylum |>
     filter(route %in% busiest_routes$route)
   
-  X_busiest_routes |>
-    ggplot(aes(year, value)) +
-    geom_line(aes(colour = route)) +
+  X_other_routes <- X_asylum |>
+    filter(route %in% other_routes$route)
+  
+  ggplot() +
+    geom_line(data = X_busiest_routes, mapping = aes(x = year, y = value, colour = route)) +
+    geom_line(data = X_other_routes, mapping = aes(x = year, y = value, group = route), alpha = 0.2) +
     theme(legend.position = "none") +
     geom_text_repel(
-      data = function(x) x |> filter(year == "2022-01-01"),
-      aes(label = route, colour = route),
+      mapping = aes(x = year, y = value, label = route, colour = route),
+      data = function(x) X_busiest_routes |> filter(year == "2022-01-01"),
       fontface ="plain",
       size = 5,
       direction = "y",
       hjust = 0,
-      segment.size = 2,
-      segment.alpha = .5,
-      segment.linetype = "dotted",
-      box.padding = .4,
-      segment.curvature = -0.1,
-      segment.ncp = 3,
-      segment.angle = 20
+      segment.size = 0,
+      box.padding = .1
     ) +
     scale_x_date(
       expand = c(0, 0),
