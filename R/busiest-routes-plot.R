9ade8e0339c5d69251685817135da1ab35d3a313
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-11-01 17:30:42 +0000

    add title, subtitle and caption (#34)

--- R/busiest-routes-plot.R
+++ R/busiest-routes-plot.R
@@ -24,6 +24,11 @@ busiest_routes_plot <- function(X) {
   X_other_routes <- X_asylum |>
     filter(route %in% other_routes$route)
   
+  plot_short_description <- "Busiest routes for asylum seekers"
+  plot_methodology_note <- "Numbers of asylum seekers with known country of origin and application"
+  data_credit <- "United Nations High Commissioner for Refugees (UNHCR) Refugee Data Finder"
+  plot_credit <- "Matthew Henderson"
+  
   ggplot() +
     geom_line(data = X_busiest_routes, mapping = aes(x = year, y = value, colour = route)) +
     geom_line(data = X_other_routes, mapping = aes(x = year, y = value, group = route), linewidth = 0.1, alpha = 0.5) +
@@ -31,7 +36,6 @@ busiest_routes_plot <- function(X) {
     geom_text_repel(
       mapping = aes(x = year, y = value, label = route, colour = route),
       data = function(x) X_busiest_routes |> filter(year == "2022-01-01"),
-      fontface ="plain",
       size = 5,
       direction = "x",
       hjust = 0,
@@ -42,6 +46,11 @@ busiest_routes_plot <- function(X) {
     scale_x_date(
       expand = c(0, 0),
       limits = c(as.Date("2010-01-01"), as.Date("2025-01-01"))
+    ) +
+    labs(
+      title = plot_short_description,
+      subtitle = plot_methodology_note,
+      caption = paste("Data:", data_credit, "\nPlot:", plot_credit)
     )
 
 }
